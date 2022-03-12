a0545c27c339ad2f355f65d3e28d0f2a2e076e5c
wicheril <snajper.snajp@gmail.com>
2022-03-12 20:34:05 +0300

    Fixed LoadPage and add new Icons

--- FUTURE.Android/MainActivity.cs
+++ FUTURE.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace FUTURE.Droid
 {
-    [Activity(Label = "FUTURE", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "FUTURE", Icon = "@mipmap/icon", /*Theme = "@style/MainTheme"*/ Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
